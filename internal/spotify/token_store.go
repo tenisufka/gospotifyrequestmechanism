b1564059ea43c201dba133bd4cb4f5dcea009ad4
Tenisowka <80459775+tenisufka@users.noreply.github.com>
2026-06-13 20:32:57 +0000

    aktualizacja

---
+++ internal/spotify/token_store.go
@@ -0,0 +1,30 @@
+package spotify
+
+import (
+	"sync"
+
+	"golang.org/x/oauth2"
+)
+
+type TokenStore struct {
+	mu    sync.RWMutex
+	token *oauth2.Token
+}
+
+func NewTokenStore() *TokenStore {
+	return &TokenStore{}
+}
+
+func (s *TokenStore) Set(token *oauth2.Token) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.token = token
+}
+
+func (s *TokenStore) Get() *oauth2.Token {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	return s.token
+}
